36659791f74c23697597a5240447c0337b762246
suu-suzu <lingchuanmeicai@gmail.com>
2023-12-05 11:02:26 +0900

    記事投稿作成画面

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ use App\Http\Controllers\PostController;
 Route::get('/', function () { return view('welcome'); });
 
 Route::get('/', [PostController::class, 'index']); 
-
+Route::get('posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
