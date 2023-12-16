acf8d9a1393d3f32a0d41b33b5a7136a345f7bf7
suu-suzu <lingchuanmeicai@gmail.com>
2023-12-16 13:44:32 +0900

    リレーション

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\PostRequest;
 use App\Models\Post;
+use App\Models\Category;
 
 class PostController extends Controller
 {
@@ -18,12 +19,8 @@ class PostController extends Controller
         return view('posts/show')->with(['post' => $post]);
     }
     
-    public function create()
-    {
-        return view('posts/create');
-    }
-    
-    public function store(PostRequest $request, Post $post)
+
+public function store(PostRequest $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
@@ -47,5 +44,10 @@ class PostController extends Controller
     {
         $post->delete();
         return redirect('/');
+    }
+    
+    public function create(Category $category)
+    {
+        return view('posts.create')->with(['categories' => $category->get()]);
     }
 }
---
+++ app/Models/Category.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+    
+    public function posts()
+{
+    return $this->hasMany(Post::class);
+
+}
+
+   public function getByCategory(int $limit_cuont = 5)
+   {
+       return $this->posts()->with('category')->orderBy('updated_at', 'DESC')->paginate($limit_cuont);
+   }
+
+}
